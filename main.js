d8be179081bfd7f84c5dcd0d3bf6cd0882c1988a
x1nax <53822451+x1nax@users.noreply.github.com>
2020-08-31 16:41:44 +0700

    update18

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ let info = (user) => {
     document.body.appendChild(newp);
     document.body.appendChild(newImg);
     })
-    .catch(()=> document.body.innerHTML = "Такой пользователь не существует");
+    .catch(() => document.body.innerHTML = "Такой пользователь не существует");
 }
 
 info(nickName(url));
