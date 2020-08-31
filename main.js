55368a9b7132931deb467a28b1122f57b82cc34a
x1nax <53822451+x1nax@users.noreply.github.com>
2020-08-31 15:32:45 +0700

    update10

--- main.js
+++ main.js
@@ -13,25 +13,35 @@ let nickName = (link) => {
 }
 nickName(url);
 console.log(nickName(url));
+
+
 let info = (user) => {
   fetch('https://api.github.com/users/'+ user)
-    .then(res => res.json())
-      .then(res => {
-        let newImg = document.createElement("IMG");
-        let newh1 = document.createElement("h1");
-        let newp = document.createElement("p");
-        let newA = document.createElement("a");
-        newImg.src = res.avatar_url;
-        newh1.innerHTML = res.name;
-        newp.innerHTML = res.bio;
-        newA.href = res.html_url;
-        document.body.appendChild(newA);
-        newA.appendChild(newh1);
-        document.body.appendChild(newp);
-        document.body.appendChild(newImg);
-        }
-      )
-      .catch(err => document.body.innerHTML = 'Ты написал ерунду');
+  .then(res => {
+    if (res.status != 404) {
+      return res.json();
+    }
+    else {
+      let err = new Error;
+      err.res = res;
+      throw err;
+      }
+})
+  .then(res => {
+    let newImg = document.createElement("IMG");
+    let newp = document.createElement("p");
+    let newA = document.createElement("a");
+    let newh1 = document.createElement("h1");
+    newImg.src = res.avatar_url;
+    newh1.innerHTML = res.name;
+    newp.innerHTML = res.bio;
+    newA.href = res.html_url;
+    document.body.appendChild(newA);
+    newA.appendChild(newh1);
+    document.body.appendChild(newp);
+    document.body.appendChild(newImg);
+    })
+    .catch(err => document.body.innerHTML = 'Ты написал ерунду');
 }
 
 info(nickName(url));
