7133c2ef55a3ce70b941e9983d2ee7cec770d0e5
x1nax <53822451+x1nax@users.noreply.github.com>
2020-09-02 15:09:44 +0700

    update20

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+
 let url = window.location.toString();
 
-let nickName = (link) => {
+let getNickName = (link) => {
   let arr = link.split('=');
   let nick = arr[1];
   if(!nick) {
@@ -8,34 +9,48 @@ let nickName = (link) => {
   }
   return nick;
 }
+nickName = getNickName(url);
 
-let info = (user) => {
-  fetch('https://api.github.com/users/'+ user)
-  .then(res => {
-    if (res.status !== 404) {
-      return res.json();
-    }
-    else {
-      let err = new Error;
-      err.res = res;
-      throw err;
-      }
-  })
-  .then(res => {
-    let newImg = document.createElement("IMG");
-    let newp = document.createElement("p");
-    let newA = document.createElement("a");
-    let newh1 = document.createElement("h1");
-    newImg.src = res.avatar_url;
-    newh1.innerHTML = res.name;
-    newp.innerHTML = res.bio;
-    newA.href = res.html_url;
-    document.body.appendChild(newA);
-    newA.appendChild(newh1);
-    document.body.appendChild(newp);
-    document.body.appendChild(newImg);
+let getInfo = new Promise((resolve, reject) => {
+  setTimeout(() => nickName ? resolve(nickName) : reject("ошибка"), 2000);
+});
+
+let now = new Date();
+let getDate = new Promise((resolve, reject) => {
+  setTimeout(() => now ? resolve(now) : reject("ошибка"), 2000);
+});
+
+Promise.all ([getDate, getInfo])
+    .then((now) => {
+      let newTime = document.createElement("p");
+      newTime.innerHTML = now;
+      document.body.appendChild(newTime);
     })
-    .catch(() => document.body.innerHTML = "Такой пользователь не существует");
-}
-
-info(nickName(url));
+    .then(nickName => {
+      fetch('https://api.github.com/users/'+ nickName)
+      .then(res => {
+        if (res.status !== 404) {
+          return res.json();
+        }
+        else {
+          let err = new Error;
+          err.res = res;
+          throw err;
+          }
+      })
+      .then(res => {
+        let newImg = document.createElement("IMG");
+        let newp = document.createElement("p");
+        let newA = document.createElement("a");
+        let newh1 = document.createElement("h1");
+        newImg.src = res.avatar_url;
+        newh1.innerHTML = res.name;
+        newp.innerHTML = res.bio;
+        newA.href = res.html_url;
+        document.body.appendChild(newA);
+        newA.appendChild(newh1);
+        document.body.appendChild(newp);
+        document.body.appendChild(newImg);
+        })
+        .catch(() => document.body.innerHTML = "Такой пользователь не существует");
+    });
